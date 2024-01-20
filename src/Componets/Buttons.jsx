b02ba4be461d81351919c6457dbcbe71be1a19dc
Aditya Kumar Sen <adityasen894@gmail.com>
2024-01-21 01:22:56 +0530

    Added width for Button

--- src/Componets/Buttons.jsx
+++ src/Componets/Buttons.jsx
@@ -1,10 +1,10 @@
 
-const Buttons = ({label, iconURL, backgroundColor, borderColor, textColor}) => {
+const Buttons = ({label, iconURL, backgroundColor, borderColor, textColor, fullWidth}) => {
   return (
     <button className={`flex gap-2 px-7 py-4 border font-monserrat text-lg leading-none  rounded-full  items-center justify center
     ${backgroundColor ?  `${backgroundColor} ${textColor} ${borderColor}` :
    "bg-coral-red  text-white  border-coral-red"}
-     w-max`}>
+     ${fullWidth && w-max}`}>
         {label}
 
         {iconURL && <img src={iconURL} alt="Right pointing arrow" 
